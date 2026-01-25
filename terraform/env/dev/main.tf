ddce3715052562f40172af8651a6d8c6d52fc363
silasfrancis <silasfrancis07@gmail.com>
2026-01-25 04:28:03 +0100

    le tired

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -1,4 +1,13 @@
 terraform {
+  backend "remote" {
+    organization = "silasfrancis"
+
+    workspaces {
+      name = "silasfrancis-workspace"
+    }
+    
+  }
+
   required_providers {
     linode = {
       source = "linode/linode"
@@ -8,4 +17,28 @@ terraform {
 }
 
 provider "linode" {
+}
+
+locals {
+  environment = dev
+  cluster_name = k8-main-cluster
+  region = "us-lax"
+}
+
+module "vpc" {
+  source = "../../linode_modules/vpc"
+
+  vpc_name = local.cluster_name
+  region = local.region
+}
+
+module "lke" {
+  source = "../../linode_modules/lke"
+
+  cluster_name = local.cluster_name
+  k8_version = "1.34"
+  region = local.region
+  vpc_id = module.vpc.vpc_id
+  node_pool_type = "g6-standard-1"
+  node_count = 2
 }
